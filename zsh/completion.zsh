fde1bd015ae564de0e87f6024c2f0351522ec702
Black Napalm <8057455+bnapalm@users.noreply.github.com>
2021-12-15 01:37:55 +0200

    Completion

---
+++ zsh/completion.zsh
@@ -0,0 +1,4 @@
+zstyle ':completion:*' completer _expand_alias _extensions _complete _approximate
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "#XDG_CACHE_HOME/zsh/.zcompcache"
+zstyle ':completion:*' menu select=5
