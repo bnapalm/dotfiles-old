b90d849f7b9bab22c6a5908941383dbbcae34725
Black Napalm <8057455+bnapalm@users.noreply.github.com>
2021-12-19 16:57:20 +0200

    Esc interferes with normal Esc workflow

--- nvim/nvim-cmp-postconfig.lua
+++ nvim/nvim-cmp-postconfig.lua
@@ -16,7 +16,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<Esc>'] = cmp.mapping({
+    ['<C-e>'] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
